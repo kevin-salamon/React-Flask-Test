8fc53a8aa55e64c11e4884a00f5b7093f5ffbac4
Timothy Salmon <tsalmon2000@gmail.com>
2020-04-23 09:29:05 -0500

    Link up JS API client and flask server

--- http/web/client/src/utils/API.js
+++ http/web/client/src/utils/API.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
+export const FLASK_BASE_URI = 'http://localhost:4433'
+
 export const saveJob = jobData => {
   console.log("Attempting to post data to the below route:");
-  return axios.post("/api/jobs", jobData);
+  return axios.post(FLASK_BASE_URI + "/api/jobs", jobData);
 }
 
 export const getSavedJobs = () => {
   console.log("Retrieving data from below route:");
-  return axios.get("/api/jobs");
+  return axios.get(FLASK_BASE_URI + "/api/jobs");
 }
 
 export const removeJob = jobId => {
   console.log("Attepting to remove data from below route:");
-  return axios.delete(`/api/jobs/${jobId}`)
+  return axios.delete(FLASK_BASE_URI + "/api/jobs/${jobId}")
 }
 
 export default {
